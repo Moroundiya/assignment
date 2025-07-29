1b578d1a1b24189820eafc54ff7b7eec82505fd1
Moroundiya <moroundiya@outlook.com>
2025-07-29 02:49:43 +0100

    Homepage Design

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,19 +1,33 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { Link } from "react-router";
 import productImg from "../assets/test-img.webp";
+import { useState } from "react";
 
 export const Products = () => {
+	const [search, setSearch] = useState(false);
 	return (
 		<>
-			<nav className="bg-[#7A9E7E] py-4 px-3 text-black flex justify-between items-center lg:px-12">
+			<div
+				className={`bg-white border border-[#7A9E7E] z-10 shadow-md rounded-md w-11/12 mx-auto lg:w-[300px] py-2 lg:py-1.5 px-2.5 absolute left-3 lg:left-12 ${
+					search ? "top-[70px]" : "-top-[100px]"
+				} transition-all duration-1000 ease-in-out`}>
+				<input
+					type="search"
+					className="w-full h-full outline-none "
+				/>
+			</div>
+			<nav className="bg-[#7A9E7E] py-4 px-3 text-black fixed z-50 w-full top-0 left-0 flex justify-between items-center lg:px-12">
 				<Icon
-					icon="mingcute:search-line"
-					className="text-2xl"
+					icon={`${search ? "iconoir:cancel" : "mingcute:search-line"}`}
+					className="text-2xl transition-all duration-500 cursor-pointer ease-in-out"
+					onClick={() => setSearch(!search)}
 				/>
 				<h1 className="text-2xl font-bold uppercase">Products</h1>
 
 				<div className="relative">
-					<div className="w-5 h-5 bg-orange-500 text-white text-[11px] rounded-full absolute -top-2 -right-2 flex justify-center items-center">20</div>
+					<div className="w-5 h-5 bg-orange-500 text-white text-[11px] rounded-full absolute -top-2 -right-2 flex justify-center items-center">
+						20
+					</div>
 					<Icon
 						icon="grommet-icons:cart"
 						className="text-2xl"
@@ -21,9 +35,9 @@ export const Products = () => {
 				</div>
 			</nav>
 
-			<div className="mt-6 gap-6 px-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:mt-10 lg:grid-cols-4 xl:grid-cols-5 lg:px-12">
+			<div className="mt-6 gap-6 px-3 pt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:mt-10 lg:grid-cols-4 xl:grid-cols-5 lg:px-12">
 				<div className="bg-white rounded-[20px] shadow-lg p-1.5 lg:p-4 flex justify-center items-center flex-col cursor-pointer">
-					<div className='w-full bg-[#F7F7F7] rounded-[20px] flex justify-center items-center overflow-hidden relative cursor-pointer group after:content-[""] after:w-full after:h-full after:bg-[d#017d0346] after:absolute after:top-0 after:left-0'>
+					<div className="w-full bg-[#F7F7F7] rounded-[20px] flex justify-center items-center cursor-pointer group relative z-0">
 						<p className="bg-[#7A9E7E] text-white px-2.5 lg:px-3 rounded-md py-0.5 text-[10px] lg:text-[12px] absolute top-3 left-3 font-light">
 							In Stock
 						</p>
